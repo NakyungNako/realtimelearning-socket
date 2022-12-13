257fa9b11d78f47d866c98a3ae6a6e5b1ce64987
Nam Nguyen <phuongnam2804@gmail.com>
2022-12-14 03:04:59 +0700

    create socket server

---
+++ index.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: process.env.CLIENT_URL,
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  socket.on("join_room", (data) => {
+    socket.join(data);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.broadcast.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("send_data", (data) => {
+    socket.broadcast.to(data.room).emit("receive_data", data);
+  });
+});
+
+server.listen(process.env.SOCKET_PORT || 5001, () => {
+  console.log("SERVER IS RUNNING");
+});
